5b3f283047505160c848d6b065177f5f0b2c5a02
21A91A05H1 <97424381+21A91A05H1@users.noreply.github.com>
2022-05-05 10:52:29 +0530

    n= int(input()) for i in range(1,n+1):     for j in range(1,i+1):         print(j,end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,6 +1,5 @@
-n=int(input())
-for i in range(n,0,-1):
+n= int(input())
+for i in range(1,n+1):
     for j in range(1,i+1):
-        if j<=i:
-            print(j,end='')
+        print(j,end='')
     print()
