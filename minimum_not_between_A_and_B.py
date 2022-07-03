d99b9035c88da85b785b8e45156d62996b4c528b
21A91A05H1 <97424381+21A91A05H1@users.noreply.github.com>
2022-07-03 11:42:12 +0530

    n=int(input()) a=list(map(int,input().split())) l,r=map(int,input().split()) m=999 c=0 for i in a:     if i>=l and i<=r:         continue     if m>i:         m=i         c+=1 if c==0:     print(-1) else:     print(m)

--- minimum_not_between_A_and_B.py
+++ minimum_not_between_A_and_B.py
@@ -1,15 +1,15 @@
 n=int(input())
 a=list(map(int,input().split()))
 l,r=map(int,input().split())
-min=999
+m=999
 c=0
 for i in a:
     if i>=l and i<=r:
         continue
-    elif min>i:
-        min=i
-        c+=1  
+    if m>i:
+        m=i
+        c+=1
 if c==0:
     print(-1)
 else:
-    print(min)
+    print(m)
