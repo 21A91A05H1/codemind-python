8a4291a3bc81025747d01d6ac28a370c124368c9
21A91A05H1 <97424381+21A91A05H1@users.noreply.github.com>
2022-07-06 19:30:00 +0530

    s=int(input()) a=list(map(int,input().split())) m,n=map(int,input().split()) c=0 min=999 for i in a:     if i>=m and i<=n:          if min>i:              min=i              c+=1 if c==0:     print(-1) else:     print(min)

--- minimum_between_A_and_B.py
+++ minimum_between_A_and_B.py
@@ -1,13 +1,13 @@
-n=int(input())
+s=int(input())
 a=list(map(int,input().split()))
-l,r=map(int,input().split())
-min=999
+m,n=map(int,input().split())
 c=0
+min=999
 for i in a:
-    if i>=l and i<=r:
-        if i<=min:
-            min=i
-            c+=1
+    if i>=m and i<=n:
+         if min>i:
+             min=i
+             c+=1
 if c==0:
     print(-1)
 else:
