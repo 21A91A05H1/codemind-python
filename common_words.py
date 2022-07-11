30cd742250c89fb5fec13a9360bb2298e8e59993
21A91A05H1 <97424381+21A91A05H1@users.noreply.github.com>
2022-07-11 11:26:44 +0530

    s1=input() s2=input() s1=s1.lower() s2=s2.lower() c=0 b=[] for i in s2.split():     if i in s1.split():         if i==' ':             continue         b.append(i)         c+=1 if c==0:     print(-1) else:     print(*b)

--- common_words.py
+++ common_words.py
@@ -2,13 +2,15 @@ s1=input()
 s2=input()
 s1=s1.lower()
 s2=s2.lower()
-d=[]
 c=0
+b=[]
 for i in s2.split():
     if i in s1.split():
+        if i==' ':
+            continue
+        b.append(i)
         c+=1
-        d.append(i)
-if(c==0):
-    print('-1')
+if c==0:
+    print(-1)
 else:
-    print(*d)
+    print(*b)
