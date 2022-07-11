67187fe179def012e17d48a19a2666cb8d77d75b
21A91A05H1 <97424381+21A91A05H1@users.noreply.github.com>
2022-07-11 11:32:06 +0530

    s1=input() s2=input() s1=s1.lower() s2=s2.lower() c=0 b=[] for i in s1:     if i in s2:         if i==' ':             continue         if ord(i) not  in b:             b.append(ord(i))             c+=1 if c==0:     print(-1) else:     b.sort()     for i in b:         print(chr(i),end='')

--- common_characters.py
+++ common_characters.py
@@ -2,13 +2,13 @@ s1=input()
 s2=input()
 s1=s1.lower()
 s2=s2.lower()
-b=[]
 c=0
+b=[]
 for i in s1:
-    if i  in s2:
-        if ord(i) not in b:
-            if i==' ':
-                continue
+    if i in s2:
+        if i==' ':
+            continue
+        if ord(i) not  in b:
             b.append(ord(i))
             c+=1
 if c==0:
