0c9e6b29badb2ee0d073d2dfce6e8d42df48806f
21A91A05H1 <97424381+21A91A05H1@users.noreply.github.com>
2022-07-11 10:19:22 +0530

    s=input() a='aeiou' b=[] for i in s:     if i==' ':         continue     if i in a:         b.append(i) c=0 for i in a:     if i not in b:             c=1             break if c==0:     print('True') else:     print('False')

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -2,14 +2,18 @@ s=input()
 a='aeiou'
 b=[]
 for i in s:
+    if i==' ':
+        continue
     if i in a:
         b.append(i)
 c=0
 for i in a:
     if i not in b:
-        c=1
-        break
+            c=1
+            break
 if c==0:
     print('True')
 else:
     print('False')
+   
+    
