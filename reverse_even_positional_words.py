60e1131f4505f0d96dcb326a050cd7bc0c4a2a4d
21A91A05H1 <97424381+21A91A05H1@users.noreply.github.com>
2022-07-12 19:58:05 +0530

    s=input() d=[] for i in s.split():     d.append(i) l=[] c=-1 for i in d:     c+=1     if c%2==0:         l.append(i[::-1])     else:         l.append(i) print(*l)

--- reverse_even_positional_words.py
+++ reverse_even_positional_words.py
@@ -1,10 +1,13 @@
 s=input()
-b=[]
-c=-1
+d=[]
 for i in s.split():
+    d.append(i)
+l=[]
+c=-1
+for i in d:
     c+=1
-    if(c%2==0):
-        b.append(i[::-1])
+    if c%2==0:
+        l.append(i[::-1])
     else:
-        b.append(i)
-print(*b)
+        l.append(i)
+print(*l)
