2ce9d2fe4920b47b89450c0e30a92ff696fd425e
21A91A05H1 <97424381+21A91A05H1@users.noreply.github.com>
2022-10-16 10:23:15 +0530

    t=int(input()) while(t):     n=int(input())     i=0     d=0     b=0     while(n):         d=d+(n%10)*(pow(8,i))         n=n//10         i=i+1     i=1     while(d):         b=b+(d%2)*i         d=d//2         i=i*10     print(b)     t-=1

---
+++ Octal_to_Binary.py
@@ -0,0 +1,17 @@
+t=int(input())
+while(t):
+    n=int(input())
+    i=0
+    d=0
+    b=0
+    while(n):
+        d=d+(n%10)*(pow(8,i))
+        n=n//10
+        i=i+1
+    i=1
+    while(d):
+        b=b+(d%2)*i
+        d=d//2
+        i=i*10
+    print(b)
+    t-=1
